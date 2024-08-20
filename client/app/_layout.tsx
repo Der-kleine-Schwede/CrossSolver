40b035f7659b7ce8813d1f3c080fad01a9cca8a8
AdrianMeDev <adrianmeinken@pm.me>
2024-08-20 11:08:45 +0200

    [DEV] add tamagui to project

---
+++ client/app/_layout.tsx
@@ -0,0 +1,46 @@
+import {
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
+} from "@react-navigation/native";
+import { useFonts } from "expo-font";
+import { Stack } from "expo-router";
+import * as SplashScreen from "expo-splash-screen";
+import { useEffect } from "react";
+import "react-native-reanimated";
+import { TamaguiProvider, View } from "@tamagui/core";
+import { Button } from "tamagui";
+import { useColorScheme } from "@/hooks/useColorScheme";
+
+import tamaguiConfig from "@/tamagui.config"; // your configuration
+// Prevent the splash screen from auto-hiding before asset loading is complete.
+SplashScreen.preventAutoHideAsync();
+
+export default function RootLayout() {
+  const colorScheme = useColorScheme();
+  const [loaded] = useFonts({
+    SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
+  });
+
+  useEffect(() => {
+    if (loaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded]);
+
+  if (!loaded) {
+    return null;
+  }
+
+  return (
+    <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
+      <Stack>
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen name="+not-found" />
+      </Stack>
+      <TamaguiProvider config={tamaguiConfig}>
+        <Button size="$6">Lorem ipsum</Button>
+      </TamaguiProvider>
+    </ThemeProvider>
+  );
+}
