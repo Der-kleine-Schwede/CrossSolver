6bfc4d1bbaf73ab6652408d978896ad451b57982
aous-afk <abomarselsyr@gmail.com>
2024-08-21 14:18:30 +0200

    [DEV] Add Csharp project.

---
+++ CoressSolverBackEnd/Domain/Problem.cs
@@ -0,0 +1,9 @@
+﻿using Domain.Common;
+
+namespace Domain {
+    public class Problem {
+        private Direction _direction;
+        private char[] _values;
+        private string _question { get; set; }
+    }
+}
